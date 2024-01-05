f08ecff9ac2615557897ceeca86845375d9f69b0
Fabio Marturano <fabiocmjor@gmail.com>
2024-01-05 14:44:29 -0300

    feat: registrar usuario e login

--- AuthJwt.Api/Controllers/AuthController.cs
+++ AuthJwt.Api/Controllers/AuthController.cs
@@ -1,24 +1,68 @@
+using AuthJwt.Domain.Dtos;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthJwt.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class AuthController : ControllerBase
     {
 
-        private readonly ILogger<AuthController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AuthController(ILogger<AuthController> logger)
+        public AuthController(
+            UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
         {
-            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet("[action]")]
+        public string TestApi()
+        {
+            return $"Api testada em {DateTime.Now.ToLongDateString()}";
+            
         }
 
         [HttpPost("[action]")]
-        public bool Login()
+        public async Task<ActionResult> RegistrarUsuario([FromBody] UserDto model)
         {
-            _logger.LogInformation("Logado com Sucesso");
-            return true;
+
+            var user = new IdentityUser 
+            { 
+                UserName = model.Email, 
+                Email = model.Email,
+                EmailConfirmed = true
+            };
+
+            if (!string.Equals(model.Password, model.ConfirmPassword, StringComparison.OrdinalIgnoreCase))
+            {
+                return BadRequest(new Exception("Confime a senha"));
+            }
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            await _signInManager.SignInAsync(user, false);
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult> Login([FromBody] UserDto userInfo)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email, userInfo.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest(new Exception("Login Inválido"));
         }
     }
 }
---
+++ AuthJwt.Domain/Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthJwt.Domain.Dtos;
+
+public class UserDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
